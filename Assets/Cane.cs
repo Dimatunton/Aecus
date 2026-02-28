2e5e8978bf08b6d4164195567d8f03339cde64be
Dimatunton <aoliveros154@gmail.com>
2026-02-28 14:11:54 +0800

    asdasdasd

---
+++ Assets/Cane.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Cane : MonoBehaviour
+{
+    public SimpleSonarShader_Parent sonarScript = null;
+
+    private Rigidbody rb;
+    private bool isGrabbed = false;
+    private XRGrabInteractable grabComponent;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        grabComponent = GetComponent<XRGrabInteractable>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isGrabbed)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.MovePosition(transform.parent.position);
+            transform.localRotation = Quaternion.Euler(Vector3.zero);
+        }
+    }
+    public void selected()
+    {
+        isGrabbed = true;
+    }
+
+    public void unselected()
+    {
+        isGrabbed = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!sonarScript)
+        {
+            Debug.LogError("No Sonar Script assigned to the cane!");
+            return;
+        }
+       
+        if(grabComponent.interactorsSelecting.Count > 0)
+        {
+            sonarScript.StartSonarRing(collision.contacts[0].point, 10f);
+        }
+    }
+}
